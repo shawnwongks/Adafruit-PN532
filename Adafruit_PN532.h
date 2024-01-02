4a84e77125732746dd0142c8b6f85b953fb6ff7e
shawnwongks <144199416+shawnwongks@users.noreply.github.com>
2024-01-02 10:59:19 +0800

    Update Adafruit_PN532.h

--- Adafruit_PN532.h
+++ Adafruit_PN532.h
@@ -194,6 +194,7 @@ public:
   uint8_t ntag2xx_WritePage(uint8_t page, uint8_t *data);
   uint8_t ntag2xx_WriteNDEFURI(uint8_t uriIdentifier, char *url,
                                uint8_t dataLen);
+  byte NFC_PWD_AUTH(byte *pwd);
 
   // Help functions to display formatted text
   static void PrintHex(const byte *data, const uint32_t numBytes);
