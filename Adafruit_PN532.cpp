dfd8f68e7bdd3e52692b778f2480648bff26dc9f
shawnwongks <144199416+shawnwongks@users.noreply.github.com>
2024-01-02 10:51:08 +0800

    Update Adafruit_PN532.cpp

--- Adafruit_PN532.cpp
+++ Adafruit_PN532.cpp
@@ -78,6 +78,7 @@ byte pn532response_firmwarevers[] = {
 #define PN532_PACKBUFFSIZ 64                ///< Packet buffer size in bytes
 byte pn532_packetbuffer[PN532_PACKBUFFSIZ]; ///< Packet buffer used in various
                                             ///< transactions
+#define DEBUG_NFC_PWD_AUTH 0
 
 /**************************************************************************/
 /*!
@@ -1817,4 +1818,49 @@ void Adafruit_PN532::writecommand(uint8_t *cmd, uint8_t cmdlen) {
       ser_dev->write(packet, 8 + cmdlen);
     }
   }
+}
+
+byte Adafruit_PN532::NFC_PWD_AUTH(byte *pwd){
+byte result;
+#if DEBUG_NFC_PWD_AUTH
+Serial.print(F("NFC_PWD_AUTH : "));
+#endif
+/* Prepare the command */
+pn532_packetbuffer[0] = PN532_COMMAND_INCOMMUNICATETHRU;
+pn532_packetbuffer[1] = 0x1B;
+memcpy (pn532_packetbuffer+2, pwd, 4); // 4 bytes or 32 bit password.
+
+/* Send the command */
+result=sendCommandCheckAck(pn532_packetbuffer, 2+4);
+#if DEBUG_NFC_PWD_AUTH
+Adafruit_PN532::PrintHexChar(pn532_packetbuffer, 40);
+#endif
+if (!result)
+{
+#if DEBUG_NFC_PWD_AUTH
+Serial.println(F("Failed to receive ACK for NFC_PWD_AUTH"));
+#endif
+return 0;
+}
+/* Read the response packet 8 + 4 */
+readdata(pn532_packetbuffer, 12);
+#if DEBUG_NFC_PWD_AUTH
+Adafruit_PN532::PrintHexChar(pn532_packetbuffer, 12);
+#endif
+if (pn532_packetbuffer[7] == 0x00)
+{
+// response is 00 00 FF 05 FB D5 43 00 00 00 E8 00
+pwd[0] = pn532_packetbuffer[8];
+pwd[1] = pn532_packetbuffer[9];
+}
+else
+{
+// Is pwd is wrong we get 00 00 FF 03 FD D5 43 01 E7 00
+// Byte 8 is 01
+#if DEBUG_NFC_PWD_AUTH
+Serial.println(F("Unexpected response NFC_PWD_AUTH: "));
+#endif
+return 0;
+}
+return 1;
 }
